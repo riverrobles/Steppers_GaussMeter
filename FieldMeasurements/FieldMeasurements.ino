5fae948dd696da2958065339acc5b37692af245e
riverrobles <riverxrobles@gmail.com>
2018-05-10 22:27:34 -0700

    Updated arduino

--- FieldMeasurements/FieldMeasurements.ino
+++ FieldMeasurements/FieldMeasurements.ino
@@ -1,12 +1,18 @@
+int stp1, stp2, stp3, dir1, dir2, dir3, ms11, ms12, ms13, ms21, ms22, ms23;
 int stps[] = {stp1,stp2,stp3};
 int dirs[] = {dir1,dir2,dir3};
 int ms1s[] = {ms11,ms12,ms13};
 int ms2s[] = {ms21,ms22,ms23};
+
 int curboard;
 int field;
-
-
 String input;
+
+bool backStep();
+bool forwardStep();
+void clearSerial();
+void Pulse();
+void resetPins();
 
 void setup() {
   Serial.begin(9600);
@@ -31,10 +37,10 @@ void loop() {
     input = Serial.readString();
     float steps = input.toFloat();
     if (steps < 0.0){
-      if backStep(-steps){Serial.println("normal")};
+      if (backStep(-steps)){Serial.println("normal");};
     }
     else if (steps > 0.0){
-      if forwardStep(steps){Serial.println("normal")};
+      if (forwardStep(steps)){Serial.println("normal");};
     }
   }
   else {
@@ -57,7 +63,7 @@ bool forwardStep(float stps){
   return true;
 }
 
-bool backwardStep(float stps){
+bool backStep(float stps){
   digitalWrite(dirs[curboard],LOW);
   for (long x = 0; x < stps; x++){
     Pulse();
